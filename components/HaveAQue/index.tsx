e4acd45538246a63b26ba6659d59a26fe89a6707
paranga <paranga@gmail.com>
2024-03-11 14:48:12 +0530

    Create a new components:- Have a Question

--- components/HaveAQue/index.tsx
+++ components/HaveAQue/index.tsx
@@ -22,7 +22,7 @@ export default function Qution() {
                 name="first-name"
                 id="first-name"
                 autoComplete="given-name"
-                className="block w-auto rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-4 focus:ring-inset focus:ring-indigo-600 lg:text-sm lg:leading-6"
+                className="block w-auto rounded-md border-0 px-3.5 py-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-4 focus:ring-inset focus:ring-indigo-600 lg:text-sm lg:leading-6"
               />
             </div>
           </div></div>

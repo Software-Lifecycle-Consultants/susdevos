14cd0997d2d4b3cbc5cb978ce62f04ae9d0dc908
Chathura-Nimsara <99wakcnk@gmail.com>
2024-07-10 14:17:09 +0530

    Side Navigation Bar Completed #136

--- components/SideNavigationBar/LogOutButton.tsx
+++ components/SideNavigationBar/LogOutButton.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
 import Image from 'next/image';
+import 'remixicon/fonts/remixicon.css';
+
+interface CardProps {
+  title: string;
+  titleHandler: (value: string) => void;
+  textLogic: boolean;
+  extraStyle?:string
+}
+
+const LogOutButton: React.FC<CardProps> = ({
+  title,
+  titleHandler,
+  textLogic,
+}) => {
 
 
-const LogOutButton: React.FC = ({ }) => {
-// export default function SideNavigationBar() {
+
   return (
-    <div className="flex items-center justify-between gap-4 pl-5 border border-gray-200  hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg">
+    <div className="flex items-center justify-between gap-4 pl-5 border border-gray-200  hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg"
+      onClick={() => {
+        titleHandler(title);
+      }}
+    >
                 <Image
                     src="/Images/SideNavigationBar/avatar.png"
                     className="text-2xl text-gray-600 group-hover:text-white "
@@ -14,17 +30,12 @@ const LogOutButton: React.FC = ({ }) => {
                     width={20}
                     height={20}
                     />
-                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
+                <h3 className={`text-left text-base text-gray-800 group-hover:text-white font-semibold  transition-all ${!textLogic ? 'w-auto' : 'w-0 hidden'} `}>
                   Logout
                 </h3>
-                <Image
-                    src="/Images/SideNavigationBar/logOutButton.png"
-                    className="text-2xl text-gray-600 group-hover:text-white "
-                    // onClick={}
-                    alt="Button Image"
-                    width={20}
-                    height={20}
-                    />
+                {!textLogic && (
+                < i className="ri-logout-box-r-line"></i>
+              )}
               </div>
   );
 }
--- components/SideNavigationBar/index.tsx
+++ components/SideNavigationBar/index.tsx
@@ -1,102 +1,219 @@
-import React from 'react';
-import SideNavigationBar from './SideNavigationBar';
+'use client';
+
+import Image from 'next/image';
+import React, { useEffect, useState } from 'react';
+
 import LogOutButton from './LogOutButton';
+import Settings from './Settings';
+import SideNavigationBar from './SideNavigationBar';
+import SearchField from './SearchField';
 
-
-export default function admin() {
-    const NavigationBarData1 = [
-        {
-          title: "Home",
-          image: "/Images/SideNavigationBar/home.png",
-          alt: "Home Image",
-          button: true
-        },
-        {
-          title: "Dashboard",
-          image: "/Images/SideNavigationBar/dashboard.png",
-          alt: "Dashboard Image",
-          button: true
-        },
-        {
-          title: "Projects",
-          image: "/Images/SideNavigationBar/project.png",
-          alt: "Projects Image",
-          button: true
-        },
-        {
-          title: "Tasks",
-          image: "/Images/SideNavigationBar/task.png",
-          alt: "Task Image",
-          button: true
-        },
-        {
-          title: "Reporting",
-          image: "/Images/SideNavigationBar/report.png",
-          alt: "Reporting Image",
-          button: true
-        },
-        {
-          title: "Users",
-          image: "/Images/SideNavigationBar/user.png",
-          alt: "Users Image",
-          button: true
-        },
-      // Add more card data objects as needed
-    ];
-
-    const NavigationBarData2 = [
-      {
-        title: "Support",
-        image: "/Images/SideNavigationBar/support.png",
-        alt: "Support Image",
-        button: true
-      },
-      {
-        title: "Settings",
-        image: "/Images/SideNavigationBar/settings.png",
-        alt: "Settings Image",
-        button: true
-      }
+const Admin = () => {
+  const NavigationBarData1 = [
+    {
+      title: 'Home',
+      image: <i className="ri-home-5-line"></i>,
+      alt: 'Home Image',
+      button: true,
+      link: '/admin/home',
+    },
+    {
+      title: 'Dashboard',
+      image: <i className="ri-bar-chart-line"></i>,
+      alt: 'Dashboard Image',
+      button: true,
+      link: '/admin/home',
+    },
+    {
+      title: 'Projects',
+      image: <i className="ri-stack-line"></i>,
+      alt: 'Projects Image',
+      button: true,
+      link: '/admin/home',
+    },
+    {
+      title: 'Tasks',
+      image: <i className="ri-edit-box-line"></i>,
+      alt: 'Task Image',
+      button: true,
+      link: '/admin/home',
+    },
+    {
+      title: 'Reporting',
+      image: <i className="ri-flag-line"></i>,
+      alt: 'Reporting Image',
+      button: true,
+      link: '/admin/home',
+    },
+    {
+      title: 'Users',
+      image: <i className="ri-group-line"></i>,
+      alt: 'Users Image',
+      button: true,
+      link: '/admin/home',
+    },
     // Add more card data objects as needed
   ];
 
-  return (
+  const NavigationBarData2 = [
+    {
+      title: 'Support',
+      image: <i className="ri-lifebuoy-line"></i>,
+      alt: 'Support Image',
+      button: true,
+      link: '/admin/support',
+    },
+    {
+      title: 'Settings',
+      image: <i className="ri-settings-5-line"></i>,
+      alt: 'Settings Image',
+      button: true,
+      link: '/admin/support',
+    },
+    // Add more card data objects as needed
+  ];
 
+  const [selectedTitleDetails, setSelectedTitleDetails] = React.useState('');
+  const [selectedTitleDetailsDisplay, setSelectedTitleDetailsDisplay] =
+    React.useState<any>({ image: '', title: '' });
+
+  const [windowWidth, setWindowWidth] = useState(0);
+  const [isShowSidebar, setIsShowSidebar] = useState(windowWidth > 1024);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+      setIsShowSidebar(window.innerWidth > 1024);
+    }
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const titleChangeHandler = (value: string) => {
+    setSelectedTitleDetails(value);
+
+    if (value !== '') {
+      const selectedData: any = NavigationBarData1.filter(
+        (sValue1: any) => sValue1.title === value,
+      );
+      setSelectedTitleDetailsDisplay(selectedData[0]);
+      console.log('selected data', selectedData);
+    }
+  };
+  const titleChangeHandler1 = (value: string) => {
+    setSelectedTitleDetails(value);
+    if (value !== '') {
+      const selectedData: any = NavigationBarData2.filter(
+        (sValue1: any) => sValue1.title === value,
+      );
+      setSelectedTitleDetailsDisplay(selectedData[0]);
+      console.log('selected data', selectedData);
+    }
+  };
+
+  return (
     <div>
-      {/* <Disclosure as="nav">
-        <Disclosure.Button className="absolute top-4 right-4 inline-flex items-center peer justify-center rounded-md p-2 text-gray-800 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white group">
-          <GiHamburgerMenu
-            className="block md:hidden h-6 w-6"
-            aria-hidden="true"
-          />
-        </Disclosure.Button> */}
-        <div className="p-6 w-1/2 h-screen bg-white z-20 fixed top-0 -left-96 lg:left-0 lg:w-60  peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
-          <div className="flex flex-col justify-start align-item-center">
-            <h1 className="text-base text-center cursor-pointer font-bold text-blue-900 border-b border-gray-100 pb-4 w-full">
-              Untitled UI
-            </h1>
-            <div className="flex flex-col my-4 border-b border-gray-100 pb-4">
-              <div className="flex flex-col gap-2">
+      <div
+        className={`${windowWidth < 1024 ? 'flex' : 'hidden'}`}
+        onClick={() => setIsShowSidebar(!isShowSidebar)}
+      >
+        {isShowSidebar ? (
+          <i className="ri-menu-line"></i>
+        ) : (
+          <i className="ri-menu-line"></i>
+        )}
+      </div>
+      <div
+        className={`p-1 h-screen z-20 top-0 w-[400px] ${isShowSidebar ? 'flex' : 'hidden'} items-start justify-start
+        peer:transition ease-out delay-150 duration-200`}
+      >
+        <div
+          className={`flex flex-col justify-start align-item-center  transition-all h-full  ${selectedTitleDetails !== '' ? 'w-1/5' : 'w-full'}  `}
+        >
+          <div className="flex flex-col justify-start align-item-center ">
+            {' '}
+            <div className='flex flex-row p-4 border-b border-gray-100'>
+              <Image
+                src="/Images/SideNavigationBar/UntitledUI.png"
+                className="text-2xl text-gray-600 group-hover:text-white "
+                // onClick={}
+                alt="Button Image"
+                width={40}
+                height={20}
+              />
+              <h1 className={`text-base text-center cursor-pointer font-bold text-blue-900  w-full ${selectedTitleDetails !== '' ? 'hidden' : ''}`}
+              >
+                Untitled UI
+              </h1>
+            </div>
+            <div className={`${selectedTitleDetails !== '' ? 'hidden' : ''}`}>
+              <SearchField/>
+            </div>
+            <div className="flex flex-col my-4 border-b border-gray-100 pb-4 ">
+              <div className={`flex flex-col gap-2  ${selectedTitleDetails !== '' ? 'items-center' : ''}`}>
                 {NavigationBarData1.map((card, index) => (
-                  <SideNavigationBar key={index} title={card.title} alt={card.alt} image={card.image} button={card.button} />
+                  <SideNavigationBar
+                    key={index}
+                    title={card.title}
+                    alt={card.alt}
+                    image={card.image}
+                    button={card.button}
+                    titleHandler={titleChangeHandler}
+                    textLogic={selectedTitleDetails !== ''}
+                    link={card.title}
+                  />
                 ))}
               </div>
             </div>
             {/* support & setting  */}
-            <div className=" my-4 border-b border-gray-100 pb-4">
-                <div className="grid gap-2"> 
-                  {NavigationBarData2.map((card, index) => (
-                    <SideNavigationBar key={index} title={card.title} alt={card.alt} image={card.image} button={card.button} />
-                  ))}
-                </div>
-            </div>
-            {/* logout */}
-            <div className=" my-4">
-              <LogOutButton/>
+            <div className=" flex flex-col my-4 border-b border-gray-100 pb-4 ">
+              <div className={`flex flex-col gap-2  ${selectedTitleDetails !== '' ? 'items-center' : ''}`}>
+                {NavigationBarData2.map((card, index) => (
+                  <SideNavigationBar
+                    key={index}
+                    title={card.title}
+                    alt={card.alt}
+                    image={card.image}
+                    button={card.button}
+                    titleHandler={titleChangeHandler1}
+                    textLogic={selectedTitleDetails !== ''}
+                    link={card.title}
+                  />
+                ))}
+              </div>
             </div>
           </div>
+          {/* logout */}
+          <div className=" my-4">
+            <LogOutButton 
+              titleHandler={titleChangeHandler1}
+              textLogic={selectedTitleDetails !== ''} title={''}            />
+          </div>
         </div>
-      {/* </Disclosure> */}
+        <div
+          className={`w-1/2  h-full  ${selectedTitleDetails !== '' ? 'w-3/4' : 'w-0 opacity-0'}`}
+          onClick={() => {
+            setSelectedTitleDetails('');
+          }}
+        >
+          {' '}
+          <div className="flex gap-2 w-full  items-center">
+            {selectedTitleDetailsDisplay.image}
+
+            <h3>{selectedTitleDetailsDisplay.title}</h3>
+          </div>
+          {selectedTitleDetailsDisplay.title == 'Settings' && (
+            <div className="">
+              <Settings />
+            </div>
+          )}{' '}
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Admin;

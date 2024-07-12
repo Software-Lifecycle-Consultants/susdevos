388a8558dceff1a296a616062a7e7f89c21af015
Chathura-Nimsara <99wakcnk@gmail.com>
2024-07-12 11:12:32 +0530

    Finished the design of the Admin Role Management page #136

--- app/admin/role-management/page.tsx
+++ app/admin/role-management/page.tsx
@@ -1,8 +1,9 @@
-//import SideNavigationBar from '@/components/SideNavigationBar';
+import AdminRoleManagement from '@/components/AdminRoleManagement';
 
 export default function adminRoleManagement() {
   return (
     <div>
+      <AdminRoleManagement/>
      
     </div>
   );
